6bc09be69c5fb92c1e8d4522e78470c2e1f42e4c
David Fink <davidfink3@gmail.com>
2024-02-08 15:08:23 -0500

    Msg

--- Chess/chess/Pawn.java
+++ Chess/chess/Pawn.java
@@ -1,5 +1,25 @@
 package chess;
 
-public class Pawn {
+public class Pawn extends Piecee{
+//	ReturnPiece pce;
+//	String file;
+//	int row;
+//	ReturnPlay board;
+	
+	public Pawn(ReturnPiece pce, ReturnPlay board) {
+		super(pce, board);
+	}
+
+	@Override
+	boolean checkCheck() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	void move() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- Chess/chess/Piecee.java
+++ Chess/chess/Piecee.java
@@ -1,5 +1,18 @@
 package chess;
 
 public abstract class Piecee {
-
+	ReturnPiece pce;
+	String file;
+	int row;
+	ReturnPlay board;
+	
+	public Piecee(ReturnPiece pce, ReturnPlay board) {
+		this.pce=pce;
+		this.file = pce.pieceFile.toString();
+		this.row=pce.pieceRank;
+		this.board = board;
+	}
+	abstract boolean checkCheck();
+	abstract void move();
+	
 }
