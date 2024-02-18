d247e4e6a580e8137dbc7a5616e29adff52ddee7
David Fink <davidfink3@gmail.com>
2024-02-18 12:01:44 -0500

    C

--- Chess/chess/Pawn.java
+++ Chess/chess/Pawn.java
@@ -2,23 +2,37 @@ package chess;
 
 import java.util.ArrayList;
 
+import chess.ReturnPiece.PieceFile;
+import chess.ReturnPiece.PieceType;
+
 public class Pawn extends ReturnPiece{
-	ReturnPiece pce;
-	chess.ReturnPiece.PieceFile file;
-	int row;
-	ArrayList<ReturnPiece> board = new ArrayList<ReturnPiece>();
-	String finalFile;
-	int finalRank;
-	chess.ReturnPlay.Message m = null;
+	// Inherited fields :: 
+//	static enum PieceType {WP, WR, WN, WB, WQ, WK, 
+//	        BP, BR, BN, BB, BK, BQ};
+//	static enum PieceFile {a, b, c, d, e, f, g, h};
+//	PieceType pieceType;
+//	PieceFile pieceFile;
+//	int pieceRank;  // 1..8
 	
-	public Pawn(ReturnPiece pce, ArrayList<ReturnPiece> board, chess.ReturnPiece.PieceFile finalFile, int finalRank) 
-	{
-		this.pce = pce;
-		this.board = board;
-	}
-	public Pawn(ReturnPiece pce, ReturnPlay board) {
-		//super(pce, board);
-	}
+	
+	
+	
+//	ReturnPiece pce;
+//	chess.ReturnPiece.PieceFile file;
+//	int row;
+	ArrayList<ReturnPiece> board = (ArrayList<ReturnPiece>)Chess.piecesOnBoard.clone();
+//	String finalFile;
+	int finalRank;
+//	chess.ReturnPlay.Message m = null;
+	
+//	public Pawn(ReturnPiece pce, ArrayList<ReturnPiece> board, chess.ReturnPiece.PieceFile finalFile, int finalRank) 
+//	{
+//		this.pce = pce;
+//		this.board = board;
+//	}
+//	public Pawn(ReturnPiece pce, ReturnPlay board) {
+//		//super(pce, board);
+//	}
 	public boolean checkCheck() {
 		return false;
 	}
@@ -27,7 +41,7 @@ public class Pawn extends ReturnPiece{
 		
 	}
 	public String toString() {
-		return file + String.valueOf(row);
+		return pieceFile + String.valueOf(pieceRank);
 	}
 
 	
@@ -38,25 +52,30 @@ public class Pawn extends ReturnPiece{
 	/**
 	 * @return ReturnPlay object. message is null if validMove() == True. Updates board.
 	 */
-	public ReturnPlay move(ArrayList<ReturnPiece> board, chess.ReturnPiece.PieceFile finalFile, int finalRank) {
-		// TODO Auto-generated method stub
-		if (!checkCheck() && validMove())
-		{
-			  chess.ReturnPlay updated_board_message = new chess.ReturnPlay();
-			  
-			  int ind = board.indexOf(pce);
-			  pce.pieceFile = finalFile;
-			  pce.pieceRank = finalRank;
-			  board.set(ind, pce);
-			  
-			  updated_board_message.piecesOnBoard = board;
-			  updated_board_message.message = m;
-			  
-			  return updated_board_message;
-			  
-		}
+	public ReturnPlay move(ReturnPiece n) {
+		
+		System.out.println("this works");
 		return null;
 	}
+//	public ReturnPlay move(ArrayList<ReturnPiece> board, chess.ReturnPiece.PieceFile finalFile, int finalRank) {
+//		// TODO Auto-generated method stub
+//		if (!checkCheck() && validMove())
+//		{
+//			  chess.ReturnPlay updated_board_message = new chess.ReturnPlay();
+//			  
+//			  int ind = board.indexOf(pce);
+//			  pce.pieceFile = finalFile;
+//			  pce.pieceRank = finalRank;
+//			  board.set(ind, pce);
+//			  
+//			  updated_board_message.piecesOnBoard = board;
+//			  updated_board_message.message = m;
+//			  
+//			  return updated_board_message;
+//			  
+//		}
+//		return null;
+//	}
 }
 		
 
